40e5dafb88c0413bb758aa81e4c5bdd06eb39002
Moises Jafet Cornelio <moises@pluio.com>
2025-04-21 23:30:50 -0400

    Retrieve the details of a specific product by its ID

---
+++ retrieve_the_details_of_a_specific_product_by_its_ID.sql
@@ -0,0 +1,8 @@
+/**
+Hoodie with Pocket
+*/
+Use `kata-woocommerce`;
+SELECT p.ID, p.post_title, p.post_content, pm.meta_key, pm.meta_value
+FROM wp_posts p
+LEFT JOIN wp_postmeta pm ON p.ID = pm.post_id
+WHERE p.ID = 34;
